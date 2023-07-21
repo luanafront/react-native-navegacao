2d36bb1dd077b25d2cdb92be3ca66b8f5842ca02
luanafront <luana.lopes.rodrigues1@gmail.com>
2023-07-21 08:16:18 -0300

    navegacao Drawer

--- src/navegacao/index.js
+++ src/navegacao/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import { SafeAreaView, Text} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
+// import Drawer from "./Drawer";
 import Stack from "./Stack";
 import Tab from "./Tab";
+
 
 export default props => (
   <SafeAreaView style={{ flex: 1 }}>
     <NavigationContainer>
       {/*<Stack />*/}
       <Tab />
+      {/*<Drawer />*/}
     </NavigationContainer>
   </SafeAreaView>
 );
